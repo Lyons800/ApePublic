b01783dda84d5e249c6b93556a95c73e849690f9
apexjnr <apexjnrchannel@gmail.com>
2022-03-22 14:49:11 +0000

    add details back into parent for flagging, created look deposits
    add details back into parent for flagging, created look deposits

--- contracts/contracts/Parent.sol
+++ contracts/contracts/Parent.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.12;
 
-import "./Child.sol";
+import "./child.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
@@ -14,7 +14,6 @@ interface deployedContract {
     function alphaClaimed(uint256 tokenID) external view returns(bool);
     function betaClaimed(uint256 tokenID) external view returns(bool);
     function gammaClaimed(uint256 tokenID) external view returns(bool);
-    function getAlphaClaimed(uint256 _id ) external view returns (bool);
 }
 
 contract ParentContract {
@@ -23,13 +22,24 @@ contract ParentContract {
 
     deployedContract claimContract;
     //parents
-    ERC721Enumerable public ALPHA = ERC721Enumerable(0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8);
-    ERC721Enumerable public BETA = ERC721Enumerable(0xf8e81D47203A594245E36C48e151709F0C19fBe8);
-    ERC721Enumerable public GAMMA = ERC721Enumerable(0xD7ACd2a9FD159E69Bb102A1ca21C9a3e3A5F771B);
+    ERC721Enumerable public ALPHA = ERC721Enumerable(0x8f2495Bdc0cfe864B5098bdE25698511a1973Af7);
+    ERC721Enumerable public BETA = ERC721Enumerable(0xD7eeB4B6e97700c8dba03B897f8BA8b8ed39E1fd);
+    ERC721Enumerable public GAMMA = ERC721Enumerable(0x069225faBD2f7bEA2Fa32Bdf1A035C0311Bd7BE0);
 
+    address _FILLERWALLET = 0x000000000000000000000000000000000000dEaD;
 
     mapping (uint256 => ChildContract) public child;
-    uint count = 1;
+    uint count;
+
+    struct Details(
+        address _childAddress; // ADDRESS Child contract address
+        address _kennelOwner;, // ADDRESS Primary wallet address
+        uint256 _kennelID; // UINT256 Primary NFT ID 
+        address _apeOwner; // APE WALLET
+        uint256 _apeID; // APE TOKEN ID
+        bool filled
+    );
+    mapping (uint => Details) public children;
 
     struct _BAYC { 
         address wallet;
@@ -45,50 +55,55 @@ contract ParentContract {
     mapping (uint => _MAYC) public MAYC;
     uint MAYCcount;
 
-
-
-    constructor()  {
-
-
-    }
-
-    function depositAlphaTest(uint256 _id, address _grapes) public returns (bool){
-       bool not_x = deployedContract(_grapes).getAlphaClaimed(_id);
-
-       return not_x;
-    }
-
+    constructor() public{}
 
     //Deposit apes into contract.
-    function depositAlpha(uint256 _id) public{
-        require(claimContract.alphaClaimed(_id) == false, "Token has been claimed");
+    function depositAlpha(uint256[] _id) public{
 
-        BAYC[BAYCcount++] = _BAYC(
-            msg.sender,
-            _id
-        );
+        for(unit i=0; i < id.length; i++){
+            require(claimContract.alphaClaimed(_id[i]) == false, "Token has been claimed");
+        }
+        
+        for(unit i=0; i < id.length; i++){
+            BAYC[BAYCcount++] = _BAYC(
+                                    msg.sender,
+                                    _id[i]
+                                );
 
-        ALPHA.transferFrom(msg.sender, address(this), _id);
+            ALPHA.transferFrom(msg.sender, address(this), _id[i]);
+        }
     }
 
-    function depositBeta(uint256 _id) public{
-        require(claimContract.betaClaimed(_id) == false, "Token has been claimed");
+    function depositBeta(uint256[] _id) public{
 
-        MAYC[MAYCcount++] = _MAYC(
-            msg.sender,
-            _id
-        );
+        for(unit i=0; i < id.length; i++){
+            require(claimContract.betaClaimed(_id[i]) == false, "Token has been claimed");
+        }
+        
+        for(unit i=0; i < id.length; i++){
+            MAYC[MAYCcount++] = _MAYC(
+                                    msg.sender,
+                                    _id[i]
+                                );
 
-        BETA.transferFrom(msg.sender, address(this), _id);
+            BETA.transferFrom(msg.sender, address(this), _id[i]);
+        }
     }
 
     //Deposit KENNEL into contract.
     //Creates child contract on deposit
-    function depositGamma(uint256 _id) public{
-        require(claimContract.gammaClaimed(_id) == false, "Token has been claimed");
-        child[++count] = new ChildContract(msg.sender, _id, address(this));
+    function depositGamma(uint256[] _id) public{
+        for(unit i=0; i < id.length; i++){
+            require(claimContract.gammaClaimed(_id[i]) == false, "Token has been claimed");
+        }
+        
+        for(unit i=0; i < id.length; i++){
+            child[++count] = new ChildContract(msg.sender, _id[i], address(this));
 
-        GAMMA.transferFrom(msg.sender, address(child[count]), _id);
+            children[count] = Details(child[count],msg.sender,_id[i],_FILLERWALLET,0,false);
+
+            GAMMA.transferFrom(msg.sender, address(child[count]), _id[i]);
+        }
     }
 
     //Matching Kennels
@@ -123,6 +138,10 @@ contract ParentContract {
 
         ALPHA.transferFrom(msg.sender, address(child[_contract]), _id);
 
+        children[count]._apeOwner = msg.sender;
+        children[count]._apeID = _id;
+        children[count].filled = true;
+
         child[_contract].setType();
         child[_contract].setSecondary(msg.sender,_id);
         child[_contract].claim();
@@ -132,6 +151,10 @@ contract ParentContract {
         require(claimContract.betaClaimed(_id) == false, "Token has been claimed");
 
         BETA.transferFrom(msg.sender, address(child[_contract]), _id);
+
+        children[count]._apeOwner = msg.sender;
+        children[count]._apeID = _id;
+        children[count].filled = true;
 
         child[_contract].setSecondary(msg.sender,_id);
         child[_contract].claim();
@@ -148,8 +171,11 @@ contract ParentContract {
         BETA.safeTransferFrom(address(this), msg.sender, tokenId);
     }
     
+    function flagContract(uint256 _id) external{
+        children[count].filled = true;
+    }
+
     function setClaimContract(address _grapes) public {
-        // Contract Grapes may be deployed here instead of being passed by argument
         claimContract = deployedContract(_grapes);
     }
 }
